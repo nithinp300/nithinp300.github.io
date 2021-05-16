882b8cdb597197c1638405fdf60165660a0582ae
Nithin Pingili <nithinp300@gmail.com>
2021-05-16 18:13:42 -0500

    header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -46,7 +46,7 @@ class Header extends Component {
             </li>
             <li>
               <a className="smoothscroll" href="#portfolio">
-                Projects
+                Portfolio
               </a>
             </li>
             {/* <li>
@@ -64,10 +64,8 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>
-              I'm a {city} based <span>{occupation}</span>. {description}.
-            </h3>
+            <h1 className="responsive-headline">Hi, I'm {name}.</h1>
+            <h2>{occupation}</h2>
             <hr />
             <ul className="social">{networks}</ul>
           </div>
